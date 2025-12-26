bc22274f3e6e0502c1f52d5c37eb78f82e00f637
quunnb <quunnb@amideus.fi>
2025-12-26 23:18:06 +0200

    Add delta parameter for custom sized resize steps

--- README.md
+++ README.md
@@ -1,47 +1,51 @@
 # softresize
 
-softresize give us four functions to handle the sizing of windows. The reference in all windows (except for the windows in the borders down and right) are the mode-line (the base bar) and the right column. So `enlarge-window` will move the mode-line of the current buffer upwards, and `reduce-window` will move it downwards. Similar behavior will occur for horizontal resize.
+Acknowledgment: This package is a fork of [jdburgosr/softresize](https://github.com/jdburgosr/softresize)
 
+softresize gives us four functions to handle the sizing of windows. The reference
+in all windows (except for the windows in the borders down and right) are the
+mode-line (the base bar) and the right column. So `enlarge-window` will move the
+mode-line of the current buffer upwards, and `reduce-window` will move it
+downwards. Similar behavior will occur for horizontal resize.
 
-## Instalation
-Clone this repo in local and add the load path to Emacs init fie.
+## Installation
 
+Clone this repo add the load path to Emacs init fie.
 
 ### For newcomers
+
 I'm doing this because I remember me, when I was just starting to use Emacs ;)
 
-1. First you have to clone this repo in your local machine because it is not in MELPA or any other package archive.
-2. Add load path to your init file `(add-to-list 'load-path "/path/to/cloned/repository/softresize")`.
-3. Next, add the code in Optional configuration to your init file.
+1. First you have to clone this repo in your local machine because it is not in
+   MELPA or any other package archive.
+1. Add load path to your init file `(add-to-list 'load-path
+   "/path/to/cloned/repository/softresize")`.
+1. Next, add the code in Optional configuration to your init file.
+
 ### If Doom Emacs
 
 If you are using doom Emacs, you can paste this code to package.el file.
 
 ``` emacs-lisp
 (package! softresize
- :recipe (:host github :repo "jdburgosr/softresize"))
+ :recipe (:host github :repo "quunnb/softresize"))
 ```
 
-And then paste the code in Optional configuration in your config.el file ☺.
+And then paste the code in Configuration in your config.el file ☺.
 
-## Optional configuration
+## Configuration
 
 ``` emacs-lisp
+;; Resize windows with Meta+Shift+(hjkl)
 (require 'softresize)
-(global-set-key (kbd "C-s-<up>") 'softresize-enlarge-window)
-(global-set-key (kbd "C-s-<down>") 'softresize-reduce-window)
-(global-set-key (kbd "C-s-<right>") 'softresize-enlarge-window-horizontally)
-(global-set-key (kbd "C-s-<left>") 'softresize-reduce-window-horizontally)
-
+(global-set-key (kbd "M-K") (lambda () (interactive) (softresize-enlarge-window 8)))
+(global-set-key (kbd "M-J") (lambda () (interactive) (softresize-reduce-window 8)))
+(global-set-key (kbd "M-L") (lambda () (interactive) (softresize-enlarge-window-horizontally 8)))
+(global-set-key (kbd "M-H") (lambda () (interactive) (softresize-reduce-window-horizontally 8)))
 ```
-### Note:
-`C-s` mean Control-Super key. In Windows, Super Key is the key with the windows Logo on keyboard.
 
+Change the keybindings and delta as you please.
 
 ## Demonstration
 
-![muestra](images/test.gif "Testing")
-
-# Developing
-
-There is other four function to manage of deleting windows next to current window. In my personal configuration I move between windows with `M-arrow`, so I configure this functions in my init.et to be called with `M-S-arrow`. In this moment that functions are quite primitive because my Elisp knowledge, but work.
+![images](images/test.gif "Testing")
--- softresize.el
+++ softresize.el
@@ -1,12 +1,13 @@
 ;;; softresize.el --- Description -*- lexical-binding: t; -*-
 ;;
 ;; Copyright (C) 2023 Gidiom Gates
+;; Copyright (C) 2025 quunnb
 ;;
 ;; Author: Gidiom Gates <juandbrg@hotmail.com>
-;; Maintainer: Gidiom Gates <juandbrg@hotmail.com>
+;; Maintainer: quunnb <quunnb@amideus.fi>
 ;; Created: enero 25, 2023
-;; Modified: enero 25, 2023
-;; Version: 0.0.1
+;; Modified: 26.12.2025 by quunnb
+;; Version: 0.0.2
 ;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
 ;; Homepage: https://github.com/gidiom/window_resize_improved
 ;; Package-Requires: ((emacs "24.3"))
@@ -14,88 +15,85 @@
 ;; This file is not part of GNU Emacs.
 ;;
 ;;; Commentary:
-;;  This is my first package for GNU Emacs, are a simple set of functions to improve the beavior on resizingwindows.
+;;  This is my [Gidiom Gates] first package for GNU Emacs, are a simple set of functions to improve the beavior on resizingwindows.
 ;;
 ;;  Description:
 ;;
 ;;  softresize give us four functions to handle the sizing of windows. The reference in all windows (except for the windows in the borders down and rigth) are the down bar and the righ column. So enlargue window will move the base bar of current buffer to up, and reduce will move it to down. Similar behavior will occur for horizontal resize.
 ;;
+;;  Changes:
+;;  - Added 'delta' parameter to softresize functions to allow custom resizing steps.
+;;
 ;;; Code:
 
 
-(defun softresize-enlarge-window ()
-  "Move mode line upward.
+(defun softresize-enlarge-window (&optional delta)
+  "Move mode line upward by DELTA lines.
 Except in the base."
-  (interactive)
-  (if (and (window-in-direction 'up) (window-in-direction 'down))
-      (shrink-window 1)
-    (if (window-in-direction 'up)
-        (enlarge-window 1)
-      (shrink-window 1))))
+  (interactive "P")
+  (let ((delta (or delta 1)))
+    (if (and (window-in-direction 'up) (window-in-direction 'down))
+        (shrink-window delta)
+      (if (window-in-direction 'up)
+          (enlarge-window delta)
+        (shrink-window delta)))))
 
-
-(defun softresize-reduce-window ()
-  "Move mode line downward.
+(defun softresize-reduce-window (&optional delta)
+  "Move mode line downward by DELTA lines.
 Except in the base."
-  (interactive)
-  (if (and (window-in-direction 'up) (window-in-direction 'down))
-      (enlarge-window 1)
-    (if (window-in-direction 'up)
-        (shrink-window 1)
-      (enlarge-window 1))))
+  (interactive "P")
+  (let ((delta (or delta 1)))
+    (if (and (window-in-direction 'up) (window-in-direction 'down))
+        (enlarge-window delta)
+      (if (window-in-direction 'up)
+          (shrink-window delta)
+        (enlarge-window delta)))))
 
+(defun softresize-enlarge-window-horizontally (&optional delta)
+  "Move right column to right direction by DELTA columns.
+Except in the right side."
+  (interactive "P")
+  (let ((delta (or delta 1)))
+    (if (and (window-in-direction 'left) (window-in-direction 'right))
+        (enlarge-window delta t)
+      (if (window-in-direction 'right)
+          (enlarge-window delta t)
+        (shrink-window delta t)))))
 
-
-(defun softresize-enlarge-window-horizontally ()
-  "Move right column to right direction.
-Except in the rigth side."
-  (interactive)
-  (if (and (window-in-direction 'left) (window-in-direction 'right))
-      (enlarge-window 1 t)
-    (if (window-in-direction 'right)
-        (enlarge-window 1 t)
-      (shrink-window 1 t))))
-
-
-(defun softresize-reduce-window-horizontally ()
-  "Move right column to left direction.
-Except in the rigth side."
-  (interactive)
-  (if (and (window-in-direction 'left) (window-in-direction 'right))
-      (shrink-window 1 t)
-    (if (window-in-direction 'right)
-        (shrink-window 1 t)
-      (enlarge-window 1 t))))
-
+(defun softresize-reduce-window-horizontally (&optional delta)
+  "Move right column to left direction by DELTA columns.
+Except in the right side."
+  (interactive "P")
+  (let ((delta (or delta 1)))
+    (if (and (window-in-direction 'left) (window-in-direction 'right))
+        (shrink-window delta t)
+      (if (window-in-direction 'right)
+          (shrink-window delta t)
+        (enlarge-window delta t)))))
 
 (defun softresize-delete-window-right()
-  "Delte window in right direction."
+  "Delete window in right direction."
   (interactive)
   (if (window-in-direction 'right)
       (delete-window (window-in-direction 'right))))
 
-
 (defun softresize-delete-window-left()
-  "Delte window in left direction."
+  "Delete window in left direction."
   (interactive)
   (if (window-in-direction 'left)
       (delete-window (window-in-direction 'left))))
 
 (defun softresize-delete-window-down()
-  "Delte window in down direction."
+  "Delete window in down direction."
   (interactive)
   (if (window-in-direction 'down)
       (delete-window (window-in-direction 'down))))
 
-
 (defun softresize-delete-window-up()
-  "Delte window in up direction."
+  "Delete window in up direction."
   (interactive)
   (if (window-in-direction 'up)
       (delete-window (window-in-direction 'up))))
-
-
-
 
 
 (provide 'softresize)
